bea937b7d87ba158df8fe16347ba01bf5433b6b3
yuimorii <109933604+itsyuimorii@users.noreply.github.com>
2024-01-24 05:18:03 -0600

    column

---
+++ src/components/ColumnContainer.tsx
@@ -0,0 +1,64 @@
+import { Column } from "../types";
+
+interface Props {
+  column: Column;
+}
+
+function ColumnContainer(props: Props) {
+  const { column } = props;
+  return (
+    <div
+      className="
+      bg-columnBackgroundColor
+      w-[350px] 
+      h-[500px] 
+      max-h-[500px]
+      rounded-md
+      flex
+      flex-col"
+    >
+      {/* Column title */}
+      <div
+        className="
+      bg-mainBackgroundColor
+      text-md
+      h-[60px]
+      cursor-grab
+      rounded-md
+      rounded-b-none
+      p-3
+      font-bold
+      border-columnBackgroundColor
+      border-4
+      flex
+      items-center
+      justify-between
+      "
+      >
+        <div className="flex gap-2">
+          <div
+            className="
+          flex 
+          justify-center 
+          items-center
+          bg-columnBackgroundColor 
+          px-2 
+          py-1 
+          text-sm
+        "
+          >
+            0
+          </div>
+          {column.title}
+        </div>
+        <button>Delete</button>
+      </div>
+      {/* Column content */}
+      <div className="flex flex-grow"> Content</div>
+      {/* Column footer */}
+      <div>footer</div>
+    </div>
+  );
+}
+
+export default ColumnContainer;
---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import { Column } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
+function KanbanBoard() {
+  const [columns, setColumns] = useState<Column[]>([]);
+  console.log(columns);
+
+  return (
+    <div
+      className="m-auto 
+    flex 
+    min-h-screen 
+    w-full 
+    items-center 
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]"
+    >
+      <div className="m-auto flex gap-8">
+        <div className="flex gap-4">
+          {columns.map((column) => (
+            <div>
+              <ColumnContainer column={column} />
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            createNewColumn();
+          }}
+          className="
+          h-[60px]
+          w-[350px]
+          min-w-[350px]
+          cursor-pointer
+          rounded-lg
+          bg-mainBackgroundColor
+          border-2
+          border-columnBackgroundColor
+          p-4
+          ring-rose-500
+          hover:ring-2
+          flex
+          gap-2
+          "
+        >
+          <PlusIcon />
+          Add Column
+        </button>
+      </div>
+    </div>
+  );
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+    setColumns([...columns, columnToAdd]);
+  }
+  function generateId() {
+    return Math.floor(Math.random() * 10001);
+  }
+}
+
+export default KanbanBoard;
