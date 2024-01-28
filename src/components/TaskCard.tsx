95cf121f135f7f4e640daf7de975709eed21f78c
yuimorii <109933604+itsyuimorii@users.noreply.github.com>
2024-01-28 12:05:36 -0600

    Update

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import TrashIcon from "../icons/TrashIcon";
 import { Task } from "../types";
 
 interface Props {
@@ -5,9 +7,13 @@ interface Props {
 }
 
 function TaskCard({ task }: Props) {
+  const [mouseIsOver, setMouseIsOver] = useState(false);
   return (
     <div className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task">
       {task.content}
+      <button className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded opacity-60 hover:opacity-100">
+        <TrashIcon />
+      </button>
     </div>
   );
 }
